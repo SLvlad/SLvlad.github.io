acbccfb43b179e7fe57820a8f4327c7d26330cb2
SLvlad <74455752+SLvlad@users.noreply.github.com>
2023-10-25 16:10:09 +0300

    Delete cursova_2 directory

--- cursova_2/main_page/main.js
+++
@@ -1,9 +0,0 @@
-const nav = document.querySelector(".nav"),
- search_icon = document.querySelector("#search_icon");
-
-search_icon.addEventListener("click", () => {
-    nav.classList.toggle("openSearch");
-    if(nav.classList.contains("openSearch")){
-        search_icon.classList.replace("search-outline","")
-    }
-})
